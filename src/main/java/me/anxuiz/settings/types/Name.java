cd92d16a4321653c60c3e3ed64e7c1a3764e15dd
Steve Anton <anxuiz.nx@gmail.com>
2012-12-01 20:38:58 -0800

    Retain the @Name annotation in runtime so the EnumType can read it.

--- src/main/java/me/anxuiz/settings/types/Name.java
+++ src/main/java/me/anxuiz/settings/types/Name.java
@@ -1,5 +1,9 @@
 package me.anxuiz.settings.types;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Name {
     String value();
 }
